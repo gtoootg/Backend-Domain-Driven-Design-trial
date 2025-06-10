f23d94598ee161f4d9fce0dbe72ec0c856d01de5
michihiro.goto <michihiro.goto@techpilot.net>
2025-06-10 19:45:21 +0200

    made password value object

--- App.Domain/Model/User/UserEntity.cs
+++ App.Domain/Model/User/UserEntity.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using App.Domain.Model.Common;
 using App.Domain.Model.Country;
+using App.Domain.Values;
 
 namespace App.Domain.Model.User;
 
@@ -20,4 +21,16 @@ public class UserEntity: BaseEntity
     public CountryEntity Country { get; set; } = null!;
     
     public string CountryCode => Country.CountryCode;
+    
+    public string PasswordHash { get; set; }
+
+    public void SetPassword(string password)
+    {
+        Password = password;
+    }
+        
+    public PasswordValue Password
+    {
+        set => PasswordHash　= value.GetPasswordHash();
+    }
 }
---
+++ App.Domain/Values/PasswordValue.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CSharpFunctionalExtensions;
+
+namespace App.Domain.Values;
+
+
+
+public class PasswordValue: ValueObject
+{
+    private static int _bCryptWorkFactor = 13;
+    private string Password { get; }
+    private string PasswordHash { get; }
+    
+    private PasswordValue(string password)
+    {
+        // if (!new PasswordSpecification().IsSatisfiedBy(password)) throw new ValidationException("Password is invalid");
+
+        PasswordHash = BCrypt.Net.BCrypt.HashPassword(password.Trim(), workFactor: _bCryptWorkFactor);
+        Password = password;
+    }
+
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return PasswordHash;
+    }
+    internal string GetPasswordHash()
+    {
+        return PasswordHash;
+    }
+    
+    public static implicit operator PasswordValue(string password) => new(password);
+}
